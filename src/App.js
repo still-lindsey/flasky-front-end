56b8dd725cb0435e8f22158305824fa7d1c331f6
Lindsey Still <lindseymstill@gmail.com>
2022-06-15 03:00:37 +0900

    added dog components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-import CatList from './components/CatList'
+import DogList from './components/DogList'
 
 
 function App() {
   return (
     <main>
-      <h1>The Cat Corral</h1>
-      <CatList></CatList>
+      <h1>The Dog Corral</h1>
+      <DogList></DogList>
     </main>
   );
 }
---
+++ src/components/Dog.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Dog = () => {
+  return (
+    <li>
+      <h3>Dog Name</h3>
+      <span>Owner: Name</span>
+      <span>Tag #: 123</span>
+      <button>Add tag</button>
+    </li>
+  )
+}
+
+export default Dog;
