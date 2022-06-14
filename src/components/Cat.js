e3b2c3d22d35687c79cba6070aeb50dd85498f9d
Lindsey Still <lindseymstill@gmail.com>
2022-06-15 02:46:49 +0900

    cat comps creeated

---
+++ src/components/Cat.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Cat = () => {
+  return (
+    <li>
+      <h3>Soon to be named Cat</h3>
+      <span>Chip #: 123</span>
+      <button>add chip</button>
+    </li>
+  );
+}
+
+export default Cat
